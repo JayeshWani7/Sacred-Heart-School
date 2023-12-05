9f29f5ce3e68ede2dbd6bb3ada4fdd35a4fbcd45
sd8capricon <sd8capricon@gmail.com>
2023-12-06 04:08:15 +0530

    initial commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			colors: {
+				'primary': '#ED1E27',
+				'primary-dark': '#C11921',
+			}
+		},
+	},
+	plugins: [],
+}
