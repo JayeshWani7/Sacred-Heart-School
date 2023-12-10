ee94eaa1f7dd48305e7edd976f13a03e2180e0a5
Leona Varghese <leonavarghesewh@gmail.com>
2023-12-10 18:35:18 +0530

    Infra & Actvities HomePage

--- src/components/home/index.ts
+++ src/components/home/index.ts
@@ -1,4 +1,10 @@
 import Objective from "./Objective.astro";
 import VisionMission from "./VisionMission.astro";
 import DeskMessage from "./DeskMessage.astro";
-export { Objective, VisionMission, DeskMessage }
+import TaglineBox from "./TaglineBox.astro";
+import InfraH from "./InfraH.astro";
+import ActivitiesH from "./ActivitiesH.astro";
+import SectionTitle from "./SectionTitle.astro";
+
+export { Objective, VisionMission, DeskMessage, TaglineBox, InfraH, ActivitiesH, SectionTitle }
+
