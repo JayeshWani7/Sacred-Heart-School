da361052afe7ccf039cacc06ce3ca6928f86b3c3
sd8capricon <72347505+sd8capricon@users.noreply.github.com>
2023-12-10 16:57:22 +0530

    section title component

--- src/components/layout/index.ts
+++ src/components/layout/index.ts
@@ -2,5 +2,6 @@ import Navbar from "./Navbar.astro";
 import OutlineBtn from "./OutlineBtn.astro";
 import Section from "./Section.astro";
 import SocialBtn from "./SocialBtn.astro";
+import SectionTitle from "./section-title.astro"
 
-export { Navbar, OutlineBtn, Section, SocialBtn }
+export { Navbar, OutlineBtn, Section, SectionTitle, SocialBtn }
