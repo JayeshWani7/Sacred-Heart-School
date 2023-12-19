7dbed724a51c8c6826256b15a04a6b3900b30680
sd8capricon <sd8capricon@gmail.com>
2023-12-19 14:00:12 +0530

    Hero component, Root & Page Layouts

--- src/components/layout/index.ts
+++ src/components/layout/index.ts
@@ -1,9 +1,10 @@
 import CTA from "./cta.astro"
 import Navbar from "./Navbar.astro";
+import Hero from "./Hero.astro";
 import OutlineBtn from "./OutlineBtn.astro";
 import Section from "./Section.astro";
 import SocialBtn from "./SocialBtn.astro";
 import SectionTitle from "./section-title.astro";
 import Footer from "./Footer.astro";
 
-export { CTA, Navbar, OutlineBtn, Section, SectionTitle, SocialBtn, Footer }
+export { CTA, Hero, Navbar, OutlineBtn, Section, SectionTitle, SocialBtn, Footer }
---
+++ src/layouts/types.ts
@@ -0,0 +1,8 @@
+interface LayoutProps {
+    title: string;
+    navtext: string;
+    transparentNav?: boolean; // transparent | light
+    buttonLink: string;
+}
+
+export type { LayoutProps };
