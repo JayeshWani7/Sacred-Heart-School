8f77354b9badd2a0a541f8c207bf75243a2cea7d
Alston Fernandes <89850018+05Alston@users.noreply.github.com>
2024-01-26 23:54:43 +0530

    Added swiperjs

--- src/components/layout/index.ts
+++ src/components/layout/index.ts
@@ -6,5 +6,7 @@ import Section from "./Section.astro";
 import SocialBtn from "./SocialBtn.astro";
 import SectionTitle from "./section-title.astro";
 import Footer from "./Footer.astro";
+import Carousel from "./Carousel.astro";
+import AlternateSectionTitle from "./AlternateSectionTitle.astro";
 
-export { CTA, Hero, Navbar, OutlineBtn, Section, SectionTitle, SocialBtn, Footer }
+export { CTA, Hero, Navbar, OutlineBtn, Section, SectionTitle, SocialBtn, Footer, Carousel, AlternateSectionTitle }
