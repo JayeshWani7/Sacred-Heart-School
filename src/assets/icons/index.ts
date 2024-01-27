b3436608df77926aab871bda13e564dec67c3943
Alston Fernandes <89850018+05Alston@users.noreply.github.com>
2024-01-27 16:49:59 +0530

    Added Carousel

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -3,5 +3,7 @@ import FB from "./fb.astro"
 import LinkedIn from "./linkedin.astro"
 import Twitter from "./twitter.astro"
 import YT from "./yt.astro"
+import Next from "./Next.svg"
+import Prev from "./Prev.svg"
 
-export { Insta, FB, LinkedIn, Twitter, YT };
+export { Insta, FB, LinkedIn, Twitter, YT, Next, Prev };
