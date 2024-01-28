cd27519fba8383b2b096e2bda8c92f1e918065d0
Alston <0508alstonfernandes@gmail.com>
2024-01-29 01:00:01 +0530

    Added alternative carousel and testimonial carousel components

--- src/components/layout/index.ts
+++ src/components/layout/index.ts
@@ -7,7 +7,9 @@ import SocialBtn from "./SocialBtn.astro";
 import SectionTitle from "./section-title.astro";
 import Footer from "./Footer.astro";
 import Carousel from "./Carousel.astro";
+import AlternateCarousel from "./CarouselAlt.astro";
 import AlternateSectionTitle from "./AlternateSectionTitle.astro";
-import Testimonial from "./TestimonialCard.astro";
+import TestimonialCarousel from "./TestimonialCarousel.astro";
+import ActivityList from "./ActivityList.astro";
 
-export { CTA, Hero, Navbar, OutlineBtn, Section, SectionTitle, SocialBtn, Footer, Carousel, AlternateSectionTitle, Testimonial }
+export { CTA, Hero, Navbar, OutlineBtn, Section, SectionTitle, SocialBtn, Footer, Carousel, AlternateCarousel, AlternateSectionTitle, TestimonialCarousel, ActivityList }
