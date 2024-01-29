d06a63c86e18bdc9e5d902d34ecf3b28e715e515
Alston Fernandes <89850018+05Alston@users.noreply.github.com>
2024-01-29 22:53:02 +0530

    Updated pre-school page styles

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -3,7 +3,7 @@ import FB from "./fb.astro"
 import LinkedIn from "./linkedin.astro"
 import Twitter from "./twitter.astro"
 import YT from "./yt.astro"
-import Next from "./Next.svg"
-import Prev from "./Prev.svg"
+import Next from "./Next.svg?raw"
+import Prev from "./Prev.svg?raw"
 
 export { Insta, FB, LinkedIn, Twitter, YT, Next, Prev };
