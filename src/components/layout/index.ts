7975a73fea35b99c92731a05fecfd69d465f97d0
sd8capricon <sd8capricon@gmail.com>
2024-03-31 00:52:49 +0530

    Section & Center Title comp name change

--- src/components/layout/index.ts
+++ src/components/layout/index.ts
@@ -4,14 +4,14 @@ import Hero from "./Hero.astro";
 import OutlineBtn from "./OutlineBtn.astro";
 import Section from "./Section.astro";
 import SocialBtn from "./SocialBtn.astro";
-import SectionTitle from "./CenterTitle.astro";
+import CenterTitle from "./CenterTitle.astro";
 import Footer from "./Footer.astro";
 import Carousel from "./Carousel.astro";
 import AlternateCarousel from "./CarouselAlt.astro";
-import AlternateSectionTitle from "./SectionTitle.astro";
+import SectionTitle from "./SectionTitle.astro";
 import TestimonialCarousel from "./TestimonialCarousel.astro";
 import ActivityList from "./ActivityList.astro";
 import Accordion from "./Accordion.astro";
 import CarouselSubtext from "./CarouselSubtext.astro"
 
-export { CTA, Hero, Navbar, OutlineBtn, Section, SectionTitle, SocialBtn, Footer, Carousel, AlternateCarousel, AlternateSectionTitle, TestimonialCarousel, ActivityList, Accordion, CarouselSubtext }
+export { CTA, Hero, Navbar, OutlineBtn, Section, SectionTitle, SocialBtn, Footer, Carousel, AlternateCarousel, CenterTitle, TestimonialCarousel, ActivityList, Accordion, CarouselSubtext }
