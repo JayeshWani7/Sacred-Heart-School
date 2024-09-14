7c61e7fd9bcbb2ab34a83aaf2801473b271bd415
JayeshWani7 <wanijayesh0@gmail.com>
2024-09-14 14:20:31 +0530

    Refactor pre-primary components: replace Subjects with Assessment, update Planner styles, and enhance curriculum descriptions

--- src/components/academic/pre-primary/index.ts
+++ src/components/academic/pre-primary/index.ts
@@ -1,5 +1,5 @@
 import Curriculum from "./Curriculum.astro";
-import Subjects from "./Subjects.astro";
+import Subjects from "./Assesment.astro";
 import Planner from "./Planner.astro";
 
 export { Curriculum, Subjects, Planner };
